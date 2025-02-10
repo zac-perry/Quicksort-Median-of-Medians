e51ef06692c7e0b999dd920945868a79eb40d1fa
zperry4 <zactperry73@gmail.com>
2025-02-09 22:35:58 -0500

    update

--- README.md
+++ README.md
@@ -4,7 +4,8 @@ Quick Sort implementation using the median-of-medians pivot selection algorithm
 # TODO: 
 - [x] Makefile, general setup
 - [ ] Research more about the median-of-medians method
-- [ ] Implementation (duh) 
+- [x] Basic implementation
+- [ ] Implementation with median-of-medians
 - [ ] Update README with details, how to run, etc.
 
 ## Implementation Details
--- main.go
+++ main.go
@@ -7,11 +7,37 @@ import (
 	"strconv"
 )
 
-// TODO
-func quickSort() {}
+// calculation the pivot index
+// Then, call quicksort on the 'left' lower elements
+// Then, call quicksort on the 'right' lower elements
+func quicksort(numbers []int, low int, high int) []int {
+	if low < high {
+		pivot, numbers := partition(numbers, low, high)
+		numbers = quicksort(numbers, low, pivot-1)
+		numbers = quicksort(numbers, pivot+1, high)
+	}
+	return numbers
+}
 
-// TODO
-func partition() {}
+// partition the data, return the pivot index & numbers slice
+// Take the rightmost value as the pivot
+// From low to high, if the number is <= pivot --> swap value positions
+// Then, swap the final elements at i and high indexes, return the pivot index and modified slice
+func partition(numbers []int, low int, high int) (int, []int) {
+	pivot := numbers[high]
+
+	i := low
+	for j := low; j < high; j++ {
+		if numbers[j] <= pivot {
+			numbers[i], numbers[j] = numbers[j], numbers[i]
+			i++
+		}
+	}
+
+	numbers[i], numbers[high] = numbers[high], numbers[i]
+
+	return i, numbers
+}
 
 func main() {
 	if len(os.Args) < 2 {
@@ -42,4 +68,5 @@ func main() {
 	}
 
 	fmt.Println("Numbers before sorting: ", numbers)
+	fmt.Println("Numbers after sorting: ", quicksort(numbers, 0, len(numbers)-1))
 }
